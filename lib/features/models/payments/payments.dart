4a98de86c84c601c632ac6c89fcc2738d6f5eb79
PranjalBajaj2 <p.bajaj212@gmail.com>
2026-07-10 18:17:14 +0500

    payment details

--- lib/features/models/payments/payments.dart
+++ lib/features/models/payments/payments.dart
@@ -1,14 +1,104 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:projectpilot/core/theme/app_colors.dart';
+import 'package:projectpilot/features/auth/providers/payment_provider.dart';
+import 'package:projectpilot/shared/payment_widgets/payment_card.dart';
+import 'package:provider/provider.dart';
 
-class PaymentsScreen extends StatelessWidget {
-  const PaymentsScreen({super.key});
+
+import '../../../routes/route_names.dart';
+
+class PaymentsScreen extends StatefulWidget {
+  const  PaymentsScreen({super.key});
+
+  @override
+  State<PaymentsScreen> createState() => _PaymentsScreenState();
+}
+
+class _PaymentsScreenState extends State<PaymentsScreen> {
+  final searchController = TextEditingController();
+
+  @override
+  void dispose() {
+    searchController.dispose();
+
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        "Payments",
-        style: TextStyle(fontSize: 28),
+    final provider = context.watch<PaymentProvider>();
+
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        tooltip: "Add new payment",
+        hoverColor: AppColors.primary,
+        onPressed: () {
+          context.push(RouteNames.addPayment);
+        },
+
+        child: const Icon(Icons.add_box),
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+
+        child: Column(
+          children: [
+            TextField(
+              controller: searchController,
+
+              onChanged: provider.searchPayment,
+
+              decoration: const InputDecoration(
+                prefixIcon: Icon(Icons.search),
+
+                hintText: "Search Payment",
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            Expanded(
+              child: Builder(
+                builder: (_) {
+                  if (provider.isLoading) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                  if (provider.error != null) {
+                    return Center(
+                      child: Text(provider.error!),
+                    );
+                  }
+
+                  if (provider.payments.isEmpty) {
+                    return const Center(child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.payments_outlined,size: 100,),
+                        Text("No Payments Found",style: TextStyle(fontSize: 25),)
+                      ],
+                    ));
+                  }
+
+                  return ListView.builder(
+                    itemCount: provider.payments.length,
+                    itemBuilder: (context, index) {
+                      final payment = provider.payments[index];
+
+                      return PaymentTile(
+                          payment: payment,
+                          onEdit: () {
+                            context.push('/edit-payment', extra: payment);
+                          },
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/shared/payment_widgets/payment_card.dart
@@ -0,0 +1,377 @@
+import 'package:flutter/material.dart';
+import 'package:projectpilot/core/theme/app_colors.dart';
+import 'package:projectpilot/features/auth/models/payment_model.dart';
+import 'package:projectpilot/features/auth/providers/payment_provider.dart';
+
+import 'package:provider/provider.dart';
+
+import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
+import '../widgets/app_snackbar.dart';
+
+class PaymentTile extends StatefulWidget {
+  final PaymentModel payment;
+  final VoidCallback onEdit;
+
+  const PaymentTile({
+    super.key,
+    required this.payment,
+    required this.onEdit,
+  });
+
+  @override
+  State<PaymentTile> createState() => _PaymentTileState();
+}
+
+class _PaymentTileState extends State<PaymentTile> {
+  bool expanded = false;
+
+  String value(String? text) {
+    if (text == null) return "—";
+
+    if (text.trim().isEmpty) {
+      return "—";
+    }
+
+    return text;
+  }
+
+  Widget buildRow(String title, String? valueText) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            width: 150,
+            child: Text(
+              title,
+              style: const TextStyle(
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Text(value(valueText)),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> deleteClient() async {
+    final provider = Provider.of<PaymentProvider>(
+      context,
+      listen: false,
+    );
+
+    final confirm = await showDialog<bool>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Delete Payment"),
+          content: const Text(
+            "Are you sure you want to delete this Payment?",
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context, false);
+              },
+              child: const Text("Cancel"),
+            ),
+            FilledButton(
+              onPressed: () {
+                Navigator.pop(context, true);
+              },
+              child: const Text("Delete"),
+            ),
+          ],
+        );
+      },
+    );
+
+    if (confirm == true) {
+      await provider.deletePayment(widget.payment.id);
+
+
+      if (mounted) {
+        AppSnackbar.show(
+          context,
+          title: "Payment",
+          message: "Deleted Successfully",
+          type: ContentType.success,
+        );
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final payment = widget.payment;
+
+    return Card(
+      margin: const EdgeInsets.symmetric(
+        horizontal: 16,
+        vertical: 8,
+      ),
+      elevation: 2,
+      child: Column(
+        children: [
+          InkWell(
+            borderRadius: BorderRadius.circular(12),
+            onTap: () {
+              setState(() {
+                expanded = !expanded;
+              });
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: expanded?
+              Column(
+                children: [
+                  CircleAvatar(
+                    radius: 25,
+                    child: Text( payment.projectName != null &&
+                        payment.projectName!.isNotEmpty
+                        ? payment.projectName![0].toUpperCase()
+                        : "?",style: const TextStyle(fontSize: 24),
+                    ),
+                  ),
+
+                  const SizedBox(height: 10),
+
+                  Text(
+                    value(payment.projectName!.toUpperCase()),
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: AnimatedRotation(
+                      turns: expanded ? 0.5 : 0,
+                      duration: const Duration(milliseconds: 250),
+                      child: const Icon(
+                        Icons.keyboard_arrow_down,
+                        color: AppColors.error,
+                      ),
+                    ),
+                  ),
+                  const Divider(),
+                ],
+              )
+                  : Row(
+                children: [
+                  CircleAvatar(
+                    child: Text( payment.projectName != null &&
+                        payment.projectName!.isNotEmpty
+                        ? payment.projectName![0].toUpperCase()
+                        : "?",),
+                  ),
+                  const SizedBox(width: 14),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment:
+                      CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          value(payment.projectName),
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 17,
+                          ),
+                        ),
+                        const SizedBox(height: 4),
+                        Text(
+                          value(payment.clientName),
+                          style: TextStyle(
+                            color: Colors.grey.shade600,
+                          ),
+                        ),
+                        const SizedBox(height: 4),
+                        Text(
+                          "${payment.currency} ${payment.amount.toStringAsFixed(0)}",
+                          style: TextStyle(
+                            color: Colors.grey.shade600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  AnimatedRotation(
+                    turns: expanded ? 0.5 : 0,
+                    duration: const Duration(milliseconds: 250),
+                    child: const Icon(
+                      Icons.keyboard_arrow_down,
+                      color: AppColors.error,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+          AnimatedCrossFade(
+            duration: const Duration(milliseconds: 250),
+            crossFadeState: expanded
+                ? CrossFadeState.showSecond
+                : CrossFadeState.showFirst,
+            firstChild: const SizedBox(),
+            secondChild: Padding(
+              padding: const EdgeInsets.fromLTRB(
+                16,
+                0,
+                16,
+                16,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+
+                  buildRow("Project Name:", payment.projectName),
+
+                  buildRow("Client Name:", payment.clientName),
+
+                  buildRow(
+                    "Project Amount:",
+                    "${payment.currency} ${payment.amount.toStringAsFixed(0)}",
+                  ),
+                  buildRow(
+                    "Payment Date:",
+                    "${payment.paymentDate.toDate().day}/${payment.paymentDate.toDate().month}/${payment.paymentDate.toDate().year}",
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(
+                          width: 150,
+                          child: Text(
+                            "Payment Method:",
+                            style: TextStyle(fontWeight: FontWeight.w600),
+                          ),
+                        ),
+                        paymentChip(payment.paymentMethod)
+                      ],
+                    ),
+                  ),
+
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(
+                          width: 150,
+                          child: Text(
+                            "Payment Status:",
+                            style: TextStyle(fontWeight: FontWeight.w600),
+                          ),
+                        ),
+                        statusChip(payment.status),
+                      ],
+                    ),
+                  ),
+
+
+                  const SizedBox(height: 20),
+
+                  Row(
+                    children: [
+                      Expanded(
+                        child: OutlinedButton.icon(
+                          onPressed: widget.onEdit,
+                          icon: const Icon(Icons.edit),
+                          label: const Text("Edit"),
+                        ),
+                      ),
+
+                      const SizedBox(width: 12),
+
+                      Expanded(
+                        child: FilledButton.icon(
+                          style: FilledButton.styleFrom(
+                            backgroundColor: AppColors.error,
+                          ),
+                          onPressed: deleteClient,
+                          icon: const Icon(Icons.delete),
+                          label: const Text("Delete"),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+Widget statusChip(String status) {
+  Color color;
+
+  switch (status.toLowerCase()) {
+    case "received":
+      color = AppColors.success;
+      break;
+
+    case "pending":
+      color = AppColors.error;
+      break;
+
+    default:
+      color = Colors.orange;
+  }
+
+  return Chip(
+    backgroundColor: color.withOpacity(0.15),
+    side: BorderSide.none,
+    label: Text(
+      status,
+      style: TextStyle(
+        color: color,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+  );
+}
+Widget paymentChip(String paymentMethod) {
+  //Icon icon;
+  Color color;
+
+  switch (paymentMethod.toLowerCase()) {
+    case "Cash":
+      color = Colors.white54;
+
+    case "Jazzcash":
+      color = AppColors.error;
+      break;
+
+    case "Easypaisa":
+      color = AppColors.success;
+      break;
+
+    case "Bank Transfer":
+      color = Colors.blue;
+      break;
+
+    default:
+      color = Colors.orange;
+  }
+
+  return Chip(
+    backgroundColor: color.withOpacity(0.15),
+    side: BorderSide.none,
+    label: Text(
+      paymentMethod,
+      style: TextStyle(
+        color: color,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+  );
+}
